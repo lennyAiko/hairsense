bb4a0268a66b304304f6a1651d459b27a80c8394
Lennox Charles <lennyaiko17@gmail.com>
2024-01-30 13:32:20 +0100

    initial commit with swagger

---
+++ config/swagger.js
@@ -0,0 +1,7 @@
+/* eslint-disable quotes */
+module.exports.swaggerConfig = {
+  info: {
+    title: "Hairsense Retail API docs",
+    description: "This is the API documentation for Hairsense retail",
+  },
+};
